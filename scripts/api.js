dc8c86a4732160300f72f12424d0ebd7b95f1532
kathyleboeuf <kleboeuf@townisp.com>
2018-07-26 17:24:20 -0400

    last large commit

--- scripts/api.js
+++ scripts/api.js
@@ -1,2 +1,9 @@
 /* global $ */
 'use strict';
+
+
+
+	const api = (function () {
+		const BASE_URL = 'https://thinkful-list-api.herokuapp.com/kathy/bookmarks';
+
+	})();
--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,11 @@
 /* global $ */
 'use strict';
+
+
+
+
+
+
+$.getJSON('https://thinkful-list-api.herokuapp.com/ei-student/items', (response) => {
+  console.log('api response:', response);
+});
